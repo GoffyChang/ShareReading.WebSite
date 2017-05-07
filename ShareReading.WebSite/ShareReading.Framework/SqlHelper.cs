835e4d3aad10ed2028c411e2223891e728b85fea
王浩兴(wangahoxing) <wanghaoxing@tuhu.cn>
2017-05-07 23:42:47 +0800

    修改

---
+++ ShareReading.WebSite/ShareReading.Framework/SqlHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class SqlHelper
+    {
+        private static string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
+        {
+            //判断数据库连接状态
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            //判断是否需要事物处理
+            if (trans != null)
+                cmd.Transaction = trans;
+            cmd.CommandType = cmdType;
+            if (cmdParms != null)
+            {
+                cmd.Parameters.AddRange(cmdParms);
+            }
+        }
+        public static int ExecteNonQuery(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                //通过PrePareCommand方法将参数逐个加入到SqlCommand的参数集合中
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
+                int val = cmd.ExecuteNonQuery();
+                //清空SqlCommand中的参数列表
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+        public static SqlDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            // we use a try/catch here because if the method throws an exception we want to 
+            // close the connection throw code, because no datareader will exist, hence the 
+            // commandBehaviour.CloseConnection will not work
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
+                    SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    cmd.Parameters.Clear();
+                    return rdr;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static DataSet ExecuteDataSet(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters);
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    DataSet ds = new DataSet();
+                    da.SelectCommand = cmd;
+                    da.Fill(ds);
+                    return ds;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters);
+                object val = cmd.ExecuteScalar();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+    }
+}
